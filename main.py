b7541be54caf1b3d918b0f5085cacf0d3bbf451c
EmileDerain <emilederain@gmail.com>
2024-02-04 16:32:53 +0100

    add

--- main.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpRequest, HttpResponse
-
-def xss_fonction(request: HttpRequest) -> HttpResponse:
-    data = str(request.GET.get("data", ""))
-
-    return HttpResponse(f"<p>You searched for: {data}<p>")
